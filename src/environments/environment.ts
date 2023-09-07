e97b922febd9ae92813ce22e7cc3cf30aa079ff7
Santiago Vargas N <sanvargas02@hotmail.com>
2023-09-07 15:05:44 -0500

    Primer Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'admin-dashboard-18425',
+    appId: '1:540100234389:web:86a1be5f83f9920dec5f9c',
+    storageBucket: 'admin-dashboard-18425.appspot.com',
+    apiKey: 'AIzaSyCjtSGlEWt9lynN2l3sxD6N-jbiyzLtbKA',
+    authDomain: 'admin-dashboard-18425.firebaseapp.com',
+    messagingSenderId: '540100234389',
+  },};
